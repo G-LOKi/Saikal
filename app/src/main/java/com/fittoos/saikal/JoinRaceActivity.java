16526e7620deba14f30560363f6140d88aaa28a6
AkhilKamath <akhilkamath97@gmail.com>
2021-01-16 08:45:18 +0530

    Screens - Join/Create, Enter Race done.

--- app/src/main/java/com/fittoos/saikal/JoinRaceActivity.java
+++ app/src/main/java/com/fittoos/saikal/JoinRaceActivity.java
@@ -2,13 +2,27 @@ package com.fittoos.saikal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class JoinRaceActivity extends AppCompatActivity {
+
+    private Button mButtonEnterRace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_race);
+
+        //****************** link ui elements *********************************
+        mButtonEnterRace = findViewById(R.id.button_enter_race);
+
+        //****************** onClick Listeners of buttons **********************
+        mButtonEnterRace.setOnClickListener(view -> {
+            Intent intent = new Intent(view.getContext(), MainRaceActivity.class);
+            startActivity(intent);
+        });
     }
 }
