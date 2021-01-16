b271983802b68b2d0b4f9f2a7a61e2635c145bc2
G-LOKi <garg.lokesh96@gmail.com>
2021-01-16 07:27:44 +0530

    Activities Created

---
+++ app/src/main/java/com/fittoos/saikal/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package com.fittoos.saikal;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashScreenActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen);
+
+
+
+
+    }
+
+}
