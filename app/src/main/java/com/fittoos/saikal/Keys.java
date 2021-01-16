d2e690c62f4ab65cb08151800db73068d924a73c
G-LOKi <garg.lokesh96@gmail.com>
2021-01-16 08:10:05 +0530

    Splash Screen Completed

---
+++ app/src/main/java/com/fittoos/saikal/Keys.java
@@ -0,0 +1,9 @@
+package com.fittoos.saikal;
+
+public class Keys {
+
+    public static final String USER_DETAILS = "userDetails";
+    public static final String USER_NAME = "userName";
+    public static final String IS_REGISTERED = "isRegistered";
+
+}
--- app/src/main/java/com/fittoos/saikal/SplashScreenActivity.java
+++ app/src/main/java/com/fittoos/saikal/SplashScreenActivity.java
@@ -5,10 +5,20 @@ import android.annotation.SuppressLint;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static com.fittoos.saikal.Keys.IS_REGISTERED;
+import static com.fittoos.saikal.Keys.USER_DETAILS;
+import static com.fittoos.saikal.Keys.USER_NAME;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -16,13 +26,27 @@ import android.view.View;
  */
 public class SplashScreenActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fullscreen);
 
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
 
+                boolean isRegistered = false;
+
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(USER_DETAILS, Context.MODE_PRIVATE);
+                isRegistered = sharedPreferences.getBoolean(IS_REGISTERED, false);
+
+                if(isRegistered) {
+                    startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                }else {
+                    startActivity(new Intent(getApplicationContext(), UserRegistrationActivity.class));
+                }
+            }
+        }, 5000);
 
 
     }
